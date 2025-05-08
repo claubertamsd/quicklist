8039658713c88e788ba62b2e38a3515d98c1f3a4
Claubert Santos <claubertvinicius68@gmail.com>
2025-05-08 12:13:41 -0300

    checkbox update style

--- script.js
+++ script.js
@@ -20,24 +20,29 @@ function createElementItem(item) {
   const div = document.createElement("div");
   const checkbox = document.createElement("input");
   const btnDelete = document.createElement("button");
-
+  const imgSpan = document.createElement("span");
   const img = document.createElement("img");
-
   img.src = "assets/icon-delete.svg";
 
   btnDelete.append(img);
 
   li.classList.add("item");
+  div.classList.add("checkbox-wrapper")
+  imgSpan.classList.add("checkbox-image")
+
 
   const span = document.createElement("span");
   span.textContent = item;
 
   checkbox.type = "checkbox";
 
-  div.append(checkbox, span);
+  div.append(checkbox, imgSpan,span);
+
 
   li.append(div, btnDelete);
   list.appendChild(li);
+
+
 
   btnDelete.addEventListener("click", () => {
     li.remove();
@@ -52,6 +57,10 @@ function errorMessage() {
   setTimeout(() => {
     trashMessage.classList.add("show");
   }, 10);
+
+  setTimeout(()=> {
+    trashMessage.classList.remove("show");
+  },3000);
 }
 
 btnClose.onclick = function() {
